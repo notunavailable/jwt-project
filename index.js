20d5199c6be55b6af63ae25ea961e24ecc1eeb99
notunavailable <77205554+notunavailable@users.noreply.github.com>
2022-01-13 13:29:20 -0800

    finished

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //https://www.section.io/engineering-education/how-to-build-authentication-api-with-jwt-token-in-nodejs/
 
+   
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const {API_PORT} = process.env;
+const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-//server listening
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
